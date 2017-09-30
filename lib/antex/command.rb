4b0d3e0f282a88cebccd126ab91927ddcb9d6271
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-09-30 19:25:02 +0200

    spec/doc/fix Command

--- lib/antex/command.rb
+++ lib/antex/command.rb
@@ -3,11 +3,39 @@
 require 'open3'
 
 module Antex
+  # This class encapsulates the execution of a command line that needs
+  # some source files to produce some target files.
+  #
+  # Existence checks are performed on files:
+  # * if all targets exist before execution then it is skipped
+  # * all sources must exist before execution
+  # * all targets must exist after execution
+  #
+  # Details of the execution itself are given by
+  # the attributes {#stdout}, {#stderr} and {#status}.
   class Command
     class MissingSourceFiles < Error; end
     class MissingTargetFiles < Error; end
     class ExecutionFailed < Error; end
 
+    # @return [String, nil] the +stdout+ returned by the command line
+    attr_reader :stdout
+
+    # @return [String, nil] the +stderr+ returned by the command line
+    attr_reader :stderr
+
+    # @return [Process::Status, nil] the +status+ returned by the command line
+    attr_reader :status
+
+    # Initializes the command.
+    #
+    # @note A command with no targets will always skip
+    #   since it needs to produce nothing!
+    #
+    # @param name [String] name of the command (just for error reporting)
+    # @param sources [Array] list of source files
+    # @param targets [Array] list of target files
+    # @param command_line [String] command line that will be executed
     def initialize(name:, sources:, targets:, command_line:)
       @name = name
       @sources = sources
@@ -15,6 +43,11 @@ module Antex
       @command_line = command_line
     end
 
+    # Executes the command.
+    #
+    # @raise [MissingSourceFiles] when source files are missing
+    # @raise [ExecutionFailed] when command execution fails
+    # @raise [MissingTargetFiles] when command does not create target files
     def run!
       return if all_exist? @targets
       check_source_files!
@@ -37,7 +70,7 @@ module Antex
 
     def check_status!
       raise ExecutionFailed, <<~EXECUTION_FAILED unless @status.success?
-        Command #{command_name} failed.
+        Command #{@name} failed.
         Command line: #{@command_line}
         Status: #{@status}
         Stdout:
---
+++ spec/command_spec.rb
@@ -0,0 +1,143 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Antex::Command do
+  let(:tmpdir) { File.join Dir.tmpdir, 'antex_tests' }
+
+  before(:each) do
+    FileUtils.rm_rf tmpdir
+    FileUtils.mkdir_p tmpdir
+  end
+
+  after(:each) do
+    FileUtils.rm_rf tmpdir
+  end
+
+  describe 'trivial command' do
+    subject do
+      described_class.new name: 'foobar',
+                          sources: [],
+                          targets: [],
+                          command_line: 'foobar'
+    end
+
+    it 'skips since noo targets are given' do
+      expect { subject.run! }
+        .to_not change { subject.status&.exited? }.from(nil)
+    end
+  end
+
+  describe 'simplest command' do
+    let(:hello_world) { File.join tmpdir, 'hello_world' }
+
+    subject do
+      described_class.new name: 'touch',
+                          sources: [],
+                          targets: [hello_world],
+                          command_line: "touch #{hello_world}"
+    end
+
+    it 'runs silently' do
+      expect { subject.run! }.to_not raise_exception
+    end
+
+    it 'runs effectively' do
+      expect { subject.run! }
+        .to change { subject.status&.exited? }.from(nil).to(true)
+    end
+  end
+
+  describe 'sources checking (pre)' do
+    let(:sources) { %w[source_a source_b].map { |fn| File.join tmpdir, fn } }
+    let(:target) { File.join tmpdir, 'target' }
+
+    subject do
+      described_class.new name: 'touch',
+                          sources: sources,
+                          targets: [target],
+                          command_line: "touch #{target}"
+    end
+
+    it 'raises if no sources exist' do
+      expect { subject.run! }
+        .to raise_exception Antex::Command::MissingSourceFiles
+    end
+
+    it 'raises if some sources exist' do
+      `touch #{sources[0]}`
+
+      expect { subject.run! }
+        .to raise_exception Antex::Command::MissingSourceFiles
+    end
+
+    it 'runs if all sources exist' do
+      `touch #{sources.join ' '}`
+
+      expect { subject.run! }
+        .to change { subject.status&.exited? }.from(nil).to(true)
+    end
+  end
+
+  describe 'targets checking (pre)' do
+    let(:targets) { %w[target_a target_b].map { |fn| File.join tmpdir, fn } }
+
+    subject do
+      described_class.new name: 'touch',
+                          sources: [],
+                          targets: targets,
+                          command_line: "touch #{targets.join ' '}"
+    end
+
+    it 'runs if no targets exist' do
+      expect { subject.run! }
+        .to change { subject.status&.exited? }.from(nil).to(true)
+    end
+
+    it 'runs if some targets exist' do
+      `touch #{targets[0]}`
+
+      expect { subject.run! }
+        .to change { subject.status&.exited? }.from(nil).to(true)
+    end
+
+    it 'skips if all targets exist' do
+      `touch #{targets.join ' '}`
+
+      expect { subject.run! }
+        .to_not change { subject.status&.exited? }.from(nil)
+    end
+  end
+
+  describe 'execution checking' do
+    let(:target) { File.join tmpdir, 'target' }
+
+    subject do
+      described_class.new name: 'exit',
+                          sources: [],
+                          targets: [target],
+                          command_line: 'exit 99'
+    end
+
+    it 'raises an error' do
+      expect { subject.run! }
+        .to raise_exception Antex::Command::ExecutionFailed
+    end
+  end
+
+  describe 'targets checking (post)' do
+    let(:targets) { %w[target_a target_b].map { |fn| File.join tmpdir, fn } }
+
+    subject do
+      described_class.new name: 'touch',
+                          sources: [],
+                          targets: targets,
+                          command_line: "touch #{targets[0]}"
+    end
+
+    it 'raises unless all targets are created' do
+      expect { subject.run! }
+        .to raise_exception Antex::Command::MissingTargetFiles
+    end
+  end
+end

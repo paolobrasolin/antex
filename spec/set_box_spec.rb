e785b0f922aa5eb15c3b222c836937b2c38c63af
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-09-30 17:33:01 +0200

    spec for zero height tfm

--- spec/set_box_spec.rb
+++ spec/set_box_spec.rb
@@ -10,41 +10,73 @@ describe Antex::SetBox do
   describe '#load' do
     subject { described_class.new }
 
-    before do
-      allow(File).to receive(:read).and_call_original
+    context 'files are valid' do
+      before do
+        allow(File).to receive(:read).and_call_original
 
-      allow(File).to receive(:read).with('tex.yml') { <<~TEX_YML }
-        pt: 1.0pt
-        ex: 2.0pt
-        ht: 8.0pt
-        dp: 2.0pt
-        wd: 20.0pt
-      TEX_YML
+        allow(File).to receive(:read).with('tex.yml') { <<~TEX_YML }
+          pt: 1.0pt
+          ex: 2.0pt
+          ht: 8.0pt
+          dp: 2.0pt
+          wd: 20.0pt
+        TEX_YML
 
-      allow(File).to receive(:read).with('tfm.svg') { <<~TFM_SVG }
-        <?xml version='1.0' encoding='UTF-8'?>
-        <svg viewBox='0.0 0.0 200.0 100.0'
-            width='200.0pt' height='100.0pt'
-            version='1.1'
-            xmlns='http://www.w3.org/2000/svg'
-            xmlns:xlink='http://www.w3.org/1999/xlink'>
-        </svg>
-      TFM_SVG
+        allow(File).to receive(:read).with('tfm.svg') { <<~TFM_SVG }
+          <?xml version='1.0' encoding='UTF-8'?>
+          <svg viewBox='0.0 0.0 200.0 100.0'
+              width='200.0pt' height='100.0pt'
+              version='1.1'
+              xmlns='http://www.w3.org/2000/svg'
+              xmlns:xlink='http://www.w3.org/1999/xlink'>
+          </svg>
+        TFM_SVG
 
-      allow(File).to receive(:read).with('fit.svg') { <<~FIT_SVG }
-        <?xml version='1.0' encoding='UTF-8'?>
-        <svg viewBox='10.0 20.0 210.0 120.0'
-            width='200.0pt' height='100.0pt'
-            version='1.1'
-            xmlns='http://www.w3.org/2000/svg'
-            xmlns:xlink='http://www.w3.org/1999/xlink'>
-        </svg>
-      FIT_SVG
+        allow(File).to receive(:read).with('fit.svg') { <<~FIT_SVG }
+          <?xml version='1.0' encoding='UTF-8'?>
+          <svg viewBox='10.0 20.0 210.0 120.0'
+              width='200.0pt' height='100.0pt'
+              version='1.1'
+              xmlns='http://www.w3.org/2000/svg'
+              xmlns:xlink='http://www.w3.org/1999/xlink'>
+          </svg>
+        FIT_SVG
+      end
+
+      it 'loads measures from an svg file' do
+        expect { subject.load yml: 'tex.yml', tfm: 'tfm.svg', fit: 'fit.svg' }
+          .to change { subject.measures }.from({}).to(result)
+      end
     end
 
-    it 'loads measures from an svg file' do
-      expect { subject.load yml: 'tex.yml', tfm: 'tfm.svg', fit: 'fit.svg' }
-        .to change { subject.measures }.from({}).to(result)
+    context 'files have all zero measures' do
+      before do
+        allow(File).to receive(:read).and_call_original
+
+        allow(File).to receive(:read).with('tex.yml') { <<~TEX_YML }
+          ex: 1.0pt
+          ht: 0.0pt
+          dp: 0.0pt
+          wd: 0.0pt
+        TEX_YML
+
+        allow(File).to receive(:read).with('tfm.svg') { <<~TFM_SVG }
+          <?xml version='1.0' encoding='UTF-8'?>
+          <svg viewBox='0 0 0 0'>
+          </svg>
+        TFM_SVG
+
+        allow(File).to receive(:read).with('fit.svg') { <<~FIT_SVG }
+          <?xml version='1.0' encoding='UTF-8'?>
+          <svg viewBox='0 0 0 0'>
+          </svg>
+        FIT_SVG
+      end
+
+      it 'raises an error due to measures' do
+        expect { subject.load yml: 'tex.yml', tfm: 'tfm.svg', fit: 'fit.svg' }
+          .to raise_exception Antex::SetBox::InvalidMeasure
+      end
     end
   end
 end

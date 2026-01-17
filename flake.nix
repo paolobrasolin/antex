068058a27cc6f8da0b9e1e11a8c5172dbe66fe77
Paolo Brasolin <paolo.brasolin@gmail.com>
2026-01-17 17:08:49 +0100

    feat: setup ruby 3.4 dev env via nix flake #149

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs.nixpkgs.url = "github:Nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.ruby_3_4
+          ];
+          shellHook = ''
+            export BUNDLE_PATH="$PWD/vendor/bundle"
+            export GEM_HOME="$PWD/vendor/bundle"
+            export GEM_PATH="$GEM_HOME:$GEM_PATH"
+          '';
+        };
+      }
+    );
+}

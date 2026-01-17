10aaa8e1182951bd0502f7b93a89478c867d7904
Paolo Brasolin <paolo.brasolin@gmail.com>
2026-01-17 17:25:51 +0100

    feat: setup minimal texlive dev env via nix flake #149

--- flake.nix
+++ flake.nix
@@ -10,10 +10,20 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {inherit system;};
+        tl = pkgs.texlive.combine {
+          inherit
+            (pkgs.texlive)
+            scheme-minimal
+            latex-bin
+            dvisvgm
+            latexmk
+            ;
+        };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.ruby_3_4
+            tl
           ];
           shellHook = ''
             export BUNDLE_PATH="$PWD/vendor/bundle"
